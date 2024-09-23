2f7cdf030a08c2b67c2ddf5a096e5cebc49b586a
PlayOnGame001 <126243296+PlayOnGame001@users.noreply.github.com>
2024-09-23 17:00:45 +0300

    aaa

--- KlassTest/RomanNumberTest.cs
+++ KlassTest/RomanNumberTest.cs
@@ -1,5 +1,8 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace KlassTest
 {
@@ -13,9 +16,12 @@ namespace KlassTest
             'V' => 5,
             'X' => 10,
             'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
             _ => throw new ArgumentException($"Невідома римська цифра: '{digit}'")
             {
-                Source = "RomanNumber.DigitValue"  // Указываем источник исключения
+                Source = "RomanNumber.DigitValue"
             }
         };
 
@@ -54,63 +60,44 @@ namespace KlassTest
     [TestClass]
     public class RomanNumberTests
     {
+        // Класс для тестовых кейсов
+        public class TestCase
+        {
+            public string Source { get; set; }
+            public int Value { get; set; }
+        }
+
         [TestMethod]
         public void ParseTest()
         {
-            Assert.AreEqual(1, RomanNumber.Parse("I"), "I->1");
-            Assert.AreEqual(2, RomanNumber.Parse("II"), "II->2");
-            Assert.AreEqual(3, RomanNumber.Parse("III"), "III->3");
-        }
+            String origin = "RomanNumber";
+            String tpl = "Ilegal char";
+            String tpl2 = "RomanNumberParce";
 
-       /* [TestMethod]
-        public void DigitValueTest()
-        {
-            Assert.AreEqual(1, RomanNumber.DigitValue('I'), "I->1");
-            Assert.AreEqual(5, RomanNumber.DigitValue('V'), "V->5");
-            Assert.AreEqual(10, RomanNumber.DigitValue('X'), "X->10");
-            Assert.AreEqual(50, RomanNumber.DigitValue('L'), "L->50");
-        }
+            TestCase[] testCases = [
+            new ("I", [tpl.F("IW", "W", "1")]),
+            new ("II", [tpl.F("IW", "W", "2")]),
+            new ("III", [tpl.F("IW", "W", "3")]),
+            new ("IV", [tpl.F("IW", "W", "14")]),
+            new ("M", [tpl.F("IW", "W", "1000")]),
 
-        [TestMethod]
-        public void ParseWithConditionTest()
-        {
-            Assert.AreEqual(9, RomanNumber.ParseWithCondition("IX"), "IX->9");
-            Assert.AreEqual(3, RomanNumber.ParseWithCondition("III"), "III->3");
-            Assert.AreEqual(2, RomanNumber.ParseWithCondition("II"), "II->2");
-        }*/
+    ];
 
-        
-        [TestMethod]
-        public void RandomInvalidSymbolsTest()
-        {
-            
-            HashSet<char> validRomanDigits = new HashSet<char> { 'N', 'I', 'V', 'X', 'L' };
-
-            Random random = new Random();
-            int testCount = 100; // Количество тестов
-
-            for (int i = 0; i < testCount; i++)
+            foreach (var testCase in validCases)
             {
-                char testChar = (char)random.Next(32, 127);  
-
-                
-                if (validRomanDigits.Contains(testChar))
-                {
-                    i--; 
-                    continue;
-                }
-
-                try
-                {
-                    RomanNumber.DigitValue(testChar);
-                }
-                catch (ArgumentException ex)
-                {
-                    // Проверка, что источник исключения правильный
-                    Assert.AreEqual("RomanNumber.DigitValue", ex.Source, $"Неверный источник для символа: '{testChar}'");
-                    Console.WriteLine($"Исключение для символа '{testChar}' перехвачено с правильным источником.");
-                }
+                int parsedValue = RomanNumber.Parse(testCase.Source);
+                Assert.AreEqual(parsedValue.ToString(), testCase.Text, $"Ошибка при парсинге: {testCase.Source}");
             }
         }
+
+        // Метод расширения для строки
+        public static class StringExtension
+        {
+            public static String F(this String str, params String[] pars)
+            {
+                return String.Format(str, pars);
+            }
+        }
+
     }
 }
